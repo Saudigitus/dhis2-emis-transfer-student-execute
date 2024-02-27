b3fac6b48791dc3c5d6769e1cc304a83ca57f734
AlziraXerinda <alziraxerinda0@gmail.com>
2024-02-27 15:23:53 +0200

    [EMISWEB-104] feat: Implemented search in header filters

--- src/components/layout/header/HeaderItem.tsx
+++ src/components/layout/header/HeaderItem.tsx
@@ -6,12 +6,19 @@ import info from "../../../assets/images/headbar/info.svg"
 import { SimpleSearch } from '../../search'
 import { componentMapping } from '../../../utils/commons/componentMapping'
 import classNames from 'classnames'
+import { useRecoilState } from 'recoil'
+import { OuQueryString } from '../../../schema/headerSearchInputSchema'
 
 export default function HeaderItem(props: HeadBarTypes): React.ReactElement {
     const { label, value, placeholder, component, dataElementId, id } = props;
     const Component = (component != null) ? componentMapping[component] : null;
     const [openDropDown, setOpenDropDown] = useState<boolean>(false);
-    const onToggle = () => { setOpenDropDown(!openDropDown) }
+    const [, setStringQuery] = useRecoilState(OuQueryString);
+
+    const onToggle = () => {
+        setStringQuery(undefined)
+        setOpenDropDown(!openDropDown)
+    }
 
     return (
         <DropdownButton
--- src/components/menuItem/Item.tsx
+++ src/components/menuItem/Item.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { MenuItem } from "@dhis2/ui"
+import { MenuItem, Help } from "@dhis2/ui"
 import { MenuItemsProps } from '../../types/menu/MenuItemTypes'
 import { useParams } from '../../hooks/commons/useQueryParams';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { HeaderFieldsState } from '../../schema/headersSchema';
 import useDataElementsParamMapping from '../../hooks/dataElements/useDataElementsParamMapping';
+import { OuQueryString } from '../../schema/headerSearchInputSchema';
 
 export default function Item(props: MenuItemsProps): React.ReactElement {
     const {  menuItems, dataElementId, onToggle } = props;
+
+    const stringQuery = useRecoilValue(OuQueryString);
+    const filteredMenuItems = stringQuery
+    ? menuItems.filter(item => item.label.toLowerCase().includes(stringQuery.toLowerCase()))
+    : menuItems;
+
     const { add } = useParams();
     const [headerFields, setHeaderFields] = useRecoilState(HeaderFieldsState)
     const paramsMapping = useDataElementsParamMapping()
@@ -29,10 +36,16 @@ export default function Item(props: MenuItemsProps): React.ReactElement {
         onToggle()
     }
 
+    if ((stringQuery && !filteredMenuItems.length) || !menuItems.length) {
+        return <Help>
+            No items found
+        </Help>
+    }
+
     return (
         <>
             {
-                menuItems?.map(menuItem => (
+                filteredMenuItems?.map(menuItem => (
                     < MenuItem onClick={() => { onChange(menuItem) }} key={menuItem.value} label={menuItem.label} />
                 ))
             }
