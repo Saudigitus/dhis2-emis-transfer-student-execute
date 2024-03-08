0de225780f72d1fbaafc8838b6c31c97841e95b5
AlziraXerinda <alziraxerinda0@gmail.com>
2024-03-08 22:54:13 +0200

    EMISWEB-110 feat: Update app name

--- d2.config.js
+++ d2.config.js
@@ -1,7 +1,7 @@
 const config = {
     type: 'app',
-    name: 'SEMIS-Student-Transfer-Execute',
-    description:"SEMIS-Student-Transfer-Execute",
+    name: 'SEMIS-Transfer-Execute',
+    description:"SEMIS-Transfer-Execute",
 
     entryPoints: {
         app: './src/app/App.tsx',
