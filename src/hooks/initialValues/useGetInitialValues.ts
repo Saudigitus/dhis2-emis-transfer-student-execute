0e6250756ee93eb991fc60de9c3f98037cf3c881
AlziraXerinda <alziraxerinda0@gmail.com>
2024-03-11 14:21:23 +0200

    Fix: ignored empty url params

--- src/hooks/initialValues/useGetInitialValues.ts
+++ src/hooks/initialValues/useGetInitialValues.ts
@@ -4,6 +4,7 @@ import { useLocation } from "react-router-dom";
 import useDataElementsParamMapping from "../dataElements/useDataElementsParamMapping";
 import { useParams } from "../commons/useQueryParams";
 
+let localLocation: string = ""
 export function useGetInitialValues() {
     const location = useLocation()
     const paramsMapping = useDataElementsParamMapping();
@@ -11,16 +12,21 @@ export function useGetInitialValues() {
     const entries = location?.search?.split('?')?.[1]?.split('&')?.map((item) => item.split('='))
     const dataElementsQuerybuilder = []
     const { urlParamiters } = useParams()
+    let diff: number = 0
     
     if (entries?.length > 0) {
         for (const [key, value] of entries) {
             const keys = Object.entries(paramsMapping)
             for (const [dataElement, name] of keys) {
                 if (name.includes(key)) {
+                    if (!localLocation.includes(value)) {
+                        diff = diff + 1
+                    }
                     dataElementsQuerybuilder.push(`${dataElement}:in:${value.replace("+", " ")}`)
                 }
             }
         }
+        if (diff > 0) 
         setHeaderFields({
             attributes: [],
             dataElements: dataElementsQuerybuilder
